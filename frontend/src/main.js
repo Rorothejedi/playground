807a06904814b6c4d6eb820dea9c8dc7835df3ae
r.cabotiau@intech-so.fr <rodolphe.cabotiau@gmail.com>
2022-09-27 22:05:00 +0200

    Add vue3 frontend app. Manage first test event.

---
+++ frontend/src/main.js
@@ -0,0 +1,23 @@
+import {
+    createApp
+} from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+window.Pusher = Pusher;
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.VUE_APP_WEBSOCKETS_KEY,
+    wsHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true
+});
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount("#app");
